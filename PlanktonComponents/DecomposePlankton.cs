7478905914047da10df8670d6dc36de732876cd0
Daniel Piker <danielpiker@gmail.com>
2013-07-14 17:33:48 +0100

    Commit of first WIP version of the Plankton half-edge mesh library and GH components

---
+++ PlanktonComponents/DecomposePlankton.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using Plankton;
+
+namespace PlanktonComponents
+{
+    public class DecomposePlankton : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the DecomposePlankton class.
+        /// </summary>
+        public DecomposePlankton()
+            : base("DeconstructPlankton", "DeconstructPlankton",
+                "Decompose a plankton mesh into its topology information",
+                "Mesh", "Triangulation")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("PMesh", "PMesh", "The input PlanktonMesh to decompose", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.Register_PointParam("Vertex_Points", "V", "Vertex point positions", GH_ParamAccess.list);
+            pManager.Register_IntegerParam("Vertex_Outgoing_Halfedge", "V_He", "One of the outgoing halfedges for each vertex", GH_ParamAccess.list);
+            pManager.Register_IntegerParam("HalfEdge_StartVertex", "He_V", "The starting vertex of each halfedge", GH_ParamAccess.list);
+            pManager.Register_IntegerParam("HalfEdge_AdjacentFace", "He_F", "The face bordered by each halfedge (or -1 if it is adjacent to a boundary)", GH_ParamAccess.list);
+            pManager.Register_IntegerParam("HalfEdge_NextHalfEdge", "He_Nxt", "The next halfedge around the same face", GH_ParamAccess.list);
+            pManager.Register_IntegerParam("HalfEdge_PrevHalfEdge", "He_Prv", "The previous halfedge around the same face", GH_ParamAccess.list);
+            pManager.Register_IntegerParam("HalfEdge_Pair", "He_P", "The halfedge joining the same 2 vertices in the opposite direction", GH_ParamAccess.list);
+            pManager.Register_IntegerParam("Face_HalfEdge", "F_He", "The first halfedge of each face", GH_ParamAccess.list);              
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            P_mesh P = new P_mesh();
+            if (!DA.GetData<P_mesh>(0, ref P)) return;
+
+            List<Point3d> Positions = new List<Point3d>();
+            List<int> OutHEdge = new List<int>();
+
+            for (int i = 0; i < P.Vertices.Count; i++)
+            {
+                Positions.Add(P.Vertices[i].Position);
+                OutHEdge.Add(P.Vertices[i].OutgoingHalfEdge);
+            }
+           
+            List<int> StartV = new List<int>();
+            List<int> AdjF = new List<int>();
+            List<int> Next = new List<int>();
+            List<int> Prev = new List<int>();
+            List<int> Pair = new List<int>();
+
+            for (int i = 0; i < P.HalfEdges.Count; i++)
+            {
+                StartV.Add(P.HalfEdges[i].StartVertex);
+                AdjF.Add(P.HalfEdges[i].AdjacentFace);
+                Next.Add(P.HalfEdges[i].NextHalfEdge);
+                Prev.Add(P.HalfEdges[i].PrevHalfEdge);
+                Pair.Add(P.PairHalfEdge(i));
+            }
+     
+            List<int> FaceEdge = new List<int>();
+            for (int i = 0; i < P.Faces.Count; i++)
+            {
+                FaceEdge.Add(P.Faces[i].FirstHalfEdge);
+            }
+
+            DA.SetDataList(0, Positions);
+            DA.SetDataList(1, OutHEdge);
+
+            DA.SetDataList(2, StartV);
+            DA.SetDataList(3, AdjF);
+            DA.SetDataList(4, Next);
+            DA.SetDataList(5, Prev);
+            DA.SetDataList(6, Pair);
+
+            DA.SetDataList(7, FaceEdge);    
+
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;                
+                return PlanktonComponents.Properties.Resources.plankton_decon;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{97c28a7c-5d5a-4b3d-a935-b8730e88749b}"); }
+        }
+    }
+}
---
+++ PlanktonComponents/GHMeshToPMesh.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rhino.Geometry;
+using Grasshopper.Kernel;
+using Plankton;
+
+namespace PlanktonComponents
+{
+ 
+    public class GHMeshToPMesh : GH_Component
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public GHMeshToPMesh()
+            : base("CreatePlankton", "CreatePlankton",
+                "Create a new Plankton halfedge mesh from a Grasshopper Mesh",
+                "Mesh", "Triangulation")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {            
+            pManager.AddMeshParameter("Mesh", "M", "Input Mesh",GH_ParamAccess.item);   
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.Register_GenericParam("PlanktonMesh", "P", "Plankton Mesh");           
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+       
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Mesh M = new Mesh();          
+            if (!DA.GetData(0, ref M)) return;
+
+            P_mesh PlanktonMesh = new P_mesh(M);
+
+            DA.SetData(0, PlanktonMesh);
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return PlanktonComponents.Properties.Resources.plankton;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{047097d1-83bd-4f76-9994-dff721c24047}"); }
+        }
+    }
+}
