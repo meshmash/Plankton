77166b01189671644a0d0b34575c5b64cef1cf76
Will Pearson <will@mcneel.com>
2017-05-17 20:56:55 +0100

    Add GH_AssemblyInfo and bump version

---
+++ src/PlanktonGh/Properties/GrasshopperAssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Grasshopper.Kernel;
+
+namespace PlanktonGh
+{
+  public class PlanktonAssemblyInfo : GH_AssemblyInfo
+  {
+    public override string Name
+    {
+      get
+      {
+        return "plankton";
+      }
+    }
+
+    public override string Version
+    {
+      get
+      {
+        return "0.4.2";
+      }
+    }
+
+    public override string AuthorContact
+    {
+      get
+      {
+        return "http://www.grasshopper3d.com/group/plankton";
+      }
+    }
+
+    public override string AuthorName
+    {
+      get
+      {
+        return "Daniel Piker and Will Pearson";
+      }
+    }
+  }
+}
